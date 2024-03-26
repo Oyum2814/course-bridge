b1632d6676bc8a88e4d2eb0cca2f58477269f46f
Om <ommukherjee28@gmail.com>
2024-03-26 13:03:47 +0530

    fix:deploy

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const mainUrl="http://localhost:3000/"
+const mainUrl=process.env.PUBLIC_URL
 const fetcher = (url:string)=>axios.get(mainUrl+url).then((res)=> res.data);
 
 export default fetcher;
